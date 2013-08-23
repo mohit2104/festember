f86d28453ff325078d8b205396510fad73920f0b
Mohit Goyal <mohitgoyal2104@gmail.com>
2013-08-24 01:22:32 +0530

    sample

---
+++ user.js
@@ -0,0 +1,8 @@
+user_pref("extensions.incredibar_i.ms_url_id", "");
+user_pref("extensions.incredibar_i.upn2", "6R8RAbVebW");
+user_pref("extensions.incredibar_i.upn2n", "92825761359877316");
+user_pref("extensions.incredibar_i.productid", "26");
+user_pref("extensions.incredibar_i.installerproductid", "26");
+user_pref("extensions.incredibar_i.did", "10665");
+user_pref("extensions.incredibar_i.ppd", "t213");
+user_pref("extensions.incredibar_i.instlRef", "");
